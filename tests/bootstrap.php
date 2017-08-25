bfcf32d37ba32da2ea3db014565bdf429c733361
陈家辉 <chenjiahui@haolianluo.com>
2017-08-25 12:31:38 +0800

    style(psr2格式):

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,4 +2,3 @@
 // ensure we get report on all possible php errors
 error_reporting(-1);
 require_once(__DIR__ . '/../vendor/autoload.php');
-
--- tests/generatestringTest.php
+++ tests/generatestringTest.php
@@ -1,6 +1,8 @@
 <?php
 namespace graychen\Test;
+
 use graychen\generatestring;
+
 class generatestringTest extends \PHPUnit_Framework_TestCase
 {
     private $string;
@@ -11,31 +13,31 @@ class generatestringTest extends \PHPUnit_Framework_TestCase
     }
     public function testGetchars()
     {
-        $result = $this->string->getchars("a",10);
-        $this->assertEquals('aaaaaaaaaa',$result);
+        $result = $this->string->getchars("a", 10);
+        $this->assertEquals('aaaaaaaaaa', $result);
     }
 
     public function testGetchars_t()
     {
-        $result = $this->string->getchars_t("abc",10);
-        $this->assertEquals('abcabcabcabcabcabcabcabcabcabc',$result);
+        $result = $this->string->getchars_t("abc", 10);
+        $this->assertEquals('abcabcabcabcabcabcabcabcabcabc', $result);
     }
 
     public function testGetchars_p()
     {
-        $result = $this->string->getchars_p("abc",10);
-        $this->assertEquals('abcabcabcabcabcabcabcabcabcabc',$result);
+        $result = $this->string->getchars_p("abc", 10);
+        $this->assertEquals('abcabcabcabcabcabcabcabcabcabc', $result);
     }
 
     public function testGetcharscmp()
     {
-        $result = $this->string->getcharscmp("abc",10);
-        $this->assertEquals('abcabcabca',$result);
+        $result = $this->string->getcharscmp("abc", 10);
+        $this->assertEquals('abcabcabca', $result);
     }
 
     public function testGetchars_s()
     {
-        $result = $this->string->getchars_s("abc",10);
-        $this->assertEquals('abcabcabcabcabcabcabcabcabcabc',$result);
+        $result = $this->string->getchars_s("abc", 10);
+        $this->assertEquals('abcabcabcabcabcabcabcabcabcabc', $result);
     }
 }
